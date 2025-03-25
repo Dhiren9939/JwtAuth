fef91cfa7dc985452154e1b96e7c1c17f4dbb738
Dhiren9939 <dhiren.nmf.1@gmail.com>
2025-03-25 17:28:39 +0530

    more error handling

--- src/main/java/me/dhiren9939/jwtauth/exceptions/GlobalExceptionHandler.java
+++ src/main/java/me/dhiren9939/jwtauth/exceptions/GlobalExceptionHandler.java
@@ -1,15 +1,22 @@
 package me.dhiren9939.jwtauth.exceptions;
 
 import io.jsonwebtoken.JwtException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
     @ExceptionHandler(UserAlreadyExistsException.class)
     public ResponseEntity<Map<String, Object>> handleUserAlreadyExists(UserAlreadyExistsException e) {
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body(Map.of(
@@ -38,9 +45,23 @@ public class GlobalExceptionHandler {
         ));
     }
 
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<Map<String, Object>> handleInvalidHttpMethod(HttpRequestMethodNotSupportedException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of(
+                "error", e.getMessage()
+        ));
+    }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<Map<String, Object>> handleNoSuchResource(NoResourceFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of(
+                "error", "No such resource was found."
+        ));
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Map<String, Object>> handleUnknownExceptions(Exception e) {
-        System.out.println(e.getMessage());
+        log.warn("Unknown Exception {} has occurred.", String.valueOf(e.getClass()));
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of(
                 "error", "unknown error"
         ));
